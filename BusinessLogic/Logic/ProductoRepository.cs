bee48347737b28666d8c5ee785c5619ff62f2394
Enzo.MKT1288 <enzo.dure@rieder.com.py>
2022-02-07 14:07:53 -0300

    Commit inicial

---
+++ BusinessLogic/Logic/ProductoRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Interfaces;
+using Core.Entities;
+
+namespace BusinessLogic.Logic
+{
+    public class ProductoRepository : IProductoRepository
+    {
+        public Task<Producto> GetProductoByIdAsync(int idProducto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<Producto>> GetProductosAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Core/Entities/Producto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public class Producto
+    {
+        public int IdProducto { get; set; } // Cuando nuestra propiedad comienza con mayúscula (PascalCase), EF entiende que se trata de una llave primaria/foránea en la BD
+                                            // si el nombre de la variable contiene 'Id' se asume que es la llave primaria de la tabla
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public int Stock { get; set; }
+        public int Marca { get; set; }
+        [Column(TypeName="decimal(18,4)")]
+        public decimal Precio { get; set; }
+        public string Imagen { get; set; }
+
+    }
+}
